d4914ed1c8fe68c9db6471c6b8dd8bc4158bd4ae
martirosyangrig <grigor.martirosyan2000@mail.ru>
2023-03-27 09:32:09 +0400

    added protected guards by user role

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -3,6 +3,8 @@ import { ApiOperation } from '@nestjs/swagger';
 import { ApiResponse } from '@nestjs/swagger/dist';
 import { ApiTags } from '@nestjs/swagger/dist/decorators';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
+import { Roles } from 'src/auth/roles-auth.decorator';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './entity/user.model';
 import { UsersService } from './users.service';
@@ -21,9 +23,16 @@ export class UsersController {
 
     @ApiOperation({summary: "Getting all users"})
     @ApiResponse({status: 200, type: [User]})
+    @Roles("User")
     @UseGuards(JwtAuthGuard)
     @Get()
     findAll() {
         return this.userService.getAllUsers();
+    }
+
+    @Roles("User")
+    @UseGuards(RolesGuard)
+    getByEmail(email: string) {
+        return this.userService.getUserByEmail(email)
     }
 }

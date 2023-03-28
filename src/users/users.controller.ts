a83351284935a5ddfeb5f1d7f057005276a1e9f7
martirosyangrig <grigor.martirosyan2000@mail.ru>
2023-03-28 09:35:30 +0400

    added give ban and give roles apis for Admins

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -5,6 +5,8 @@ import { ApiTags } from '@nestjs/swagger/dist/decorators';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { RolesGuard } from 'src/auth/guards/roles.guard';
 import { Roles } from 'src/auth/roles-auth.decorator';
+import { AddRoleDto } from './dto/add-role.dto';
+import { BanUserDto } from './dto/ban-user.dto';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './entity/user.model';
 import { UsersService } from './users.service';
@@ -30,9 +32,30 @@ export class UsersController {
         return this.userService.getAllUsers();
     }
 
+    @ApiOperation({summary: "Getting user by email"})
+    @ApiResponse({status: 200, type: User})
     @Roles("User")
     @UseGuards(RolesGuard)
+    @Get("/byemail")
     getByEmail(email: string) {
         return this.userService.getUserByEmail(email)
+    }
+
+    @ApiOperation({summary: "Give a role"})
+    @ApiResponse({status: 200})
+    @Roles("Admin")
+    @UseGuards(RolesGuard)
+    @Post("/role")
+    addRole(@Body() dto: AddRoleDto) {
+        return this.userService.addRole(dto)
+    }
+
+    @ApiOperation({summary: "Give ban to user"})
+    @ApiResponse({status: 200})
+    @Roles("Admin")
+    @UseGuards(RolesGuard)
+    @Post("/ban")
+    banUser(@Body() dto: BanUserDto) {
+        return this.userService.ban(dto)
     }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,6 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
 import { RolesService } from 'src/roles/roles.service';
+import { AddRoleDto } from './dto/add-role.dto';
+import { BanUserDto } from './dto/ban-user.dto';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './entity/user.model';
 
@@ -25,6 +27,25 @@ export class UsersService {
 
     async getUserByEmail(email: string) {
         const user = await this.userRepository.findOne({where: {email}, include: {all: true}});
+        return user;
+    }
+
+    async addRole(dto: AddRoleDto) {
+        const user = await this.userRepository.findByPk(dto.userId);
+        const role = await this.roleService.getRoleByValue(dto.value);
+        if(role && user) {
+            await user.$add("role", role.id);
+            return;
+        }
+        throw new HttpException("User or Role not found", HttpStatus.NOT_FOUND);
+    }
+
+    async ban(dto: BanUserDto) {
+        const user = await this.userRepository.findByPk(dto.userId);
+        if(!user) throw new HttpException("User not found", HttpStatus.NOT_FOUND);
+        user.banned = true;
+        user.banReason = dto.banReason;
+        await user.save();
         return user;
     }
 }
